e444937dddcd5cb6c2c1d46ebce0ae1ec3b93f00
Andrew Wheeler <awheeler294@gmail.com>
2024-04-04 17:19:39 -0600

    parse and print input

--- bin/cc-jq/src/main.rs
+++ bin/cc-jq/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, jq!");
+use std::io;
+
+use anyhow::Result;
+use clap::Parser;
+use json;
+
+/// Command line JSON parser
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// URL to make request to
+    query: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let input = io::read_to_string(io::stdin())?;
+    println!("");
+    println!("{}", args.query);
+
+    let data = json::parse(&input).unwrap();
+
+    println!("{:#}", data);
+
+    Ok(())
 }
